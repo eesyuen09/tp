436aaca19829f8c6281c9fe7530bce3d7aabf350
yijiechong13 <195323790+yijiechong13@users.noreply.github.com>
2025-10-13 21:44:12 +0800

    Update attendance card

--- src/main/java/seedu/address/ui/AttendanceCard.java
+++ src/main/java/seedu/address/ui/AttendanceCard.java
@@ -3,6 +3,7 @@ package seedu.address.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import seedu.address.model.person.Person;
 
@@ -11,7 +12,7 @@ import seedu.address.model.person.Person;
  * An UI component that displays attendance information of a {@code Person}.
  */
 
-public class AttendanceCard {
+public class AttendanceCard extends UiPart<Region>{
 
     private static final String FXML = "AttendanceCard.fxml";
 
@@ -30,7 +31,7 @@ public class AttendanceCard {
     public AttendanceCard(Person person) {
         super(FXML);
         this.person = person;
-        header.setText("Attendance history for " + person.getName() + " (" + person.getStudentId()) +")";
+        header.setText("Attendance history for " + person.getName() + " (" + person.getStudentId() + ")");
        // attendanceRecord.setText // put the list here
     }
 
