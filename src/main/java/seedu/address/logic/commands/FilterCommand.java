20e0aae8f51a785f55c9f1e15a41f9c4cfe3201a
roger <rogerlim799@gmail.com>
2025-10-21 19:45:58 +0800

    Modify FilterCommand

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -8,7 +8,6 @@ public abstract class FilterCommand extends Command {
     public static final String COMMAND_WORD = "filter";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters students based on criteria.\n"
-            + "Parameters: [-p | -up | -t] m/MMYY | t/TAG_NAME\n"
             + "Examples: \n"
             + "  " + COMMAND_WORD + " -p m/1025 (Filters by PAID status for Oct 2025)\n"
             + "  " + COMMAND_WORD + " -up m/1125 (Filters by UNPAID status for Nov 2025)\n"
