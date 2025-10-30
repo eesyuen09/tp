263e058df4f7fed84ee738b955bbce986485b0f4
deniseegohh <e1156128@u.nus.edu>
2025-10-30 13:14:32 +0800

    Fix performance tag bug

--- src/main/java/seedu/address/logic/commands/performance/PerfDeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/performance/PerfDeleteCommand.java
@@ -60,10 +60,6 @@ public class PerfDeleteCommand extends PerfCommand {
                 .orElseThrow(() -> new CommandException(
                         String.format(Messages.MESSAGE_STUDENT_ID_NOT_FOUND, studentId)));
 
-        if (!model.hasClassTag(classTag)) {
-            throw new CommandException(String.format(Messages.MESSAGE_TAG_NOT_FOUND, classTag.tagName));
-        }
-
         List<PerformanceNote> current = student.getPerformanceList().asUnmodifiableList();
         PerformanceList copy = new PerformanceList(new ArrayList<>(current));
 
